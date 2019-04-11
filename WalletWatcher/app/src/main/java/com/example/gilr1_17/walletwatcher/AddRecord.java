02a924a6311ddf06083cbdeb2d7ac745d61d11f0
Reid Giles <33459088+ReidGiles@users.noreply.github.com>
2019-04-11 12:37:57 +0100

    Added camera to add records receipt option

--- WalletWatcher/app/src/main/java/com/example/gilr1_17/walletwatcher/AddRecord.java
+++ WalletWatcher/app/src/main/java/com/example/gilr1_17/walletwatcher/AddRecord.java
@@ -1,7 +1,9 @@
 package com.example.gilr1_17.walletwatcher;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddRecord extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class AddRecord extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_record);
+    }
+
+    public void obBtnCameraClicked(View button)
+    {
+        startActivity(new Intent(AddRecord.this, MainActivity.class));
     }
 }
