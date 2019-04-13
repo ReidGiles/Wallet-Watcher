eb943dbd75a2539fe160479d2b8abdf5a7c167be
Reid Giles <33459088+ReidGiles@users.noreply.github.com>
2019-04-13 12:24:41 +0100

    Added google sign in

--- WalletWatcher/app/src/main/java/com/example/gilr1_17/walletwatcher/LogIn.java
+++ WalletWatcher/app/src/main/java/com/example/gilr1_17/walletwatcher/LogIn.java
@@ -1,42 +1,75 @@
 package com.example.gilr1_17.walletwatcher;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LogIn extends AppCompatActivity {
+public class LogIn extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
     private static final String TAG = "LogIn";
+    private static final int RC_SIGN_IN = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        this.findViewById(R.id.sign_in_button).setOnClickListener(this);
+
         mAuth = FirebaseAuth.getInstance();
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    public void onClick(View view)
+    {
+        if (view.getId() == R.id.sign_in_button)
+        {
+            onSignInButtonClicked();
+        }
     }
 
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
     }
 
-    private void updateUI(FirebaseUser user) {
+    private void updateUI(GoogleSignInAccount account) {
         // https://stackoverflow.com/questions/44491418/can-not-resolve-updateui-firebase
 
-        if (user != null)
+        if (account != null)
         {
         }
         else
@@ -47,7 +80,7 @@ public class LogIn extends AppCompatActivity {
 
     private void createAccount(String email, String password)
     {
-        mAuth.createUserWithEmailAndPassword(email, password)
+        /*mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -66,12 +99,12 @@ public class LogIn extends AppCompatActivity {
 
                         // ...
                     }
-                });
+                });*/
     }
 
     private void signIn(String email, String password)
     {
-        mAuth.signInWithEmailAndPassword(email, password)
+        /*mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -90,6 +123,51 @@ public class LogIn extends AppCompatActivity {
 
                         // ...
                     }
-                });
+                });*/
+    }
+
+    public void onSignInButtonClicked()
+    {
+        /*EditText email = this.findViewById(R.id.editTxtEmail);
+        EditText password = this.findViewById(R.id.editTxtPassword);
+        if (email.getText().toString().matches("") && password.getText().toString().matches(""))
+        {
+            Toast.makeText(LogIn.this, "Login fields must not be blank.",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            signIn(email.getText().toString(), password.getText().toString());
+        }*/
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
     }
 }
