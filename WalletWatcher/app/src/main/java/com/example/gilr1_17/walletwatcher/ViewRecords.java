3bd6afcf1a872d466cd16e05f156864a18584800
Reid Giles <33459088+ReidGiles@users.noreply.github.com>
2019-04-21 13:27:48 +0100

    Record deletion cleanup

--- WalletWatcher/app/src/main/java/com/example/gilr1_17/walletwatcher/ViewRecords.java
+++ WalletWatcher/app/src/main/java/com/example/gilr1_17/walletwatcher/ViewRecords.java
@@ -1,5 +1,6 @@
 package com.example.gilr1_17.walletwatcher;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,17 +8,21 @@ import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
@@ -26,12 +31,20 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ViewRecords extends AppCompatActivity {
 
     private static final String TAG = "ViewRecords";
 
     FirebaseFirestore db;
-    GoogleSignInClient mGoogleSignInClient;
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleSignInAccount account;
+    RelativeLayout relativeLayout;
+    List<QueryDocumentSnapshot> activeDocuments;
+    List<CardView> activeCards;
+    int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +52,10 @@ public class ViewRecords extends AppCompatActivity {
         setContentView(R.layout.activity_view_records);
 
         db = FirebaseFirestore.getInstance();
+        relativeLayout = findViewById(R.id.relativeLayout);
+        activeDocuments = new ArrayList<QueryDocumentSnapshot>();
+        activeCards = new ArrayList<CardView>();
+        id = 0;
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -50,12 +67,33 @@ public class ViewRecords extends AppCompatActivity {
         setSupportActionBar(toolBar);
     }
 
+    View.OnClickListener cardButtons = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            for (CardView cardView : activeCards)
+            {
+                if (cardView.getId() == view.getId())
+                {
+                    cardView.setBackgroundColor(Color.RED);
+                    for (QueryDocumentSnapshot d : activeDocuments)
+                    {
+                        if (d.getId() == cardView.getContentDescription())
+                        {
+                            deleteRecord("records", d.getId());
+                        }
+                    }
+                    updateUI();
+                }
+            }
+        }
+    };
+
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
+        account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI();
     }
 
     // https://stackoverflow.com/questions/35648913/how-to-set-menu-to-toolbar-in-android
@@ -66,72 +104,114 @@ public class ViewRecords extends AppCompatActivity {
         return true;
     }
 
-    private void updateUI(GoogleSignInAccount account) {
+    private void updateUI() {
         // https://stackoverflow.com/questions/44491418/can-not-resolve-updateui-firebase
 
         if (account != null)
         {
-            db.collection("records")
-                    .whereEqualTo("ownerID", account.getId())
-                    .get()
-                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                            if (task.isSuccessful()) {
-                                int cardMargin = 0;
-                                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-                                for (QueryDocumentSnapshot document : task.getResult()) {
-                                    //Log.d(TAG, document.getId() + " => " + document.getData());
-
-                                    //_rtnText1.append(document.getData().toString() + "\n\n");
-
-                                    //Log.d(TAG,"String value: " + document.getString("name"));
-
-                                    RelativeLayout relativeLayout = findViewById(R.id.relativeLayout);
-                                    CardView cardView = new CardView(ViewRecords.this);
-                                    cardView.setLayoutParams(new CardView.LayoutParams(
-                                            CardView.LayoutParams.MATCH_PARENT,
-                                            400));
-
-                                    // https://stackoverflow.com/questions/44223471/setting-margin-programmatically-to-cardview
-                                    ViewGroup.MarginLayoutParams cardViewParams = (ViewGroup.MarginLayoutParams) cardView.getLayoutParams();
-                                    cardViewParams.setMargins(30, cardMargin, 30, 30);
-
-                                    relativeLayout.addView(cardView);
-
-                                    TextView name = new TextView(ViewRecords.this);
-                                    name.setText("Name: " + document.getString("name"));
-                                    //name.setLayoutParams(params);
-                                    cardView.addView(name);
-
-                                    TextView category = new TextView(ViewRecords.this);
-                                    category.setText("Category: " + document.getString("category"));
-                                    params.setMargins(0,50,0,0);
-                                    category.setLayoutParams(params);
-                                    cardView.addView(category);
-
-                                    TextView cost = new TextView(ViewRecords.this);
-                                    cost.setText("Cost: " + document.getString("cost"));
-                                    params.setMargins(0,100,0,0);
-                                    cost.setLayoutParams(params);
-                                    cardView.addView(cost);
-
-                                    TextView description = new TextView(ViewRecords.this);
-                                    description.setText("Description: " + document.getString("description"));
-                                    params.setMargins(0,150,0,0);
-                                    description.setLayoutParams(params);
-                                    cardView.addView(description);
-
-                                    cardMargin += 450;
-                                }
-                            } else {
-                                Log.d(TAG, "Error getting documents: ", task.getException());
-                            }
-                        }
-                    });
+            populate();
         }
         else
         {
         }
+    }
+
+    private void deleteRecord(String collection, String document)
+    {
+        db.collection(collection).document(document)
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getApplicationContext(),"Document deleted",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(),"Error deleting document",Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void populate()
+    {
+        db.collection("records")
+                .whereEqualTo("ownerID", account.getId())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            int cardMargin = 0;
+                            relativeLayout.removeAllViews();
+                            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                boolean duplicate = false;
+
+                                CardView cardView = new CardView(ViewRecords.this);
+                                cardView.setLayoutParams(new CardView.LayoutParams(
+                                        CardView.LayoutParams.MATCH_PARENT,
+                                        400));
+                                cardView.setId(id + 1);
+
+                                // https://stackoverflow.com/questions/44223471/setting-margin-programmatically-to-cardview
+                                ViewGroup.MarginLayoutParams cardViewParams = (ViewGroup.MarginLayoutParams) cardView.getLayoutParams();
+                                cardViewParams.setMargins(30, cardMargin, 30, 30);
+
+                                relativeLayout.addView(cardView);
+                                cardView.setContentDescription(document.getId());
+                                activeCards.add(cardView);
+
+                                TextView name = new TextView(ViewRecords.this);
+                                name.setText("Name: " + document.getString("name"));
+                                //name.setLayoutParams(params);
+                                cardView.addView(name);
+
+                                TextView category = new TextView(ViewRecords.this);
+                                category.setText("Category: " + document.getString("category"));
+                                params.setMargins(0,50,0,0);
+                                category.setLayoutParams(params);
+                                cardView.addView(category);
+
+                                TextView cost = new TextView(ViewRecords.this);
+                                cost.setText("Cost: " + document.getString("cost"));
+                                params.setMargins(0,100,0,0);
+                                cost.setLayoutParams(params);
+                                cardView.addView(cost);
+
+                                TextView description = new TextView(ViewRecords.this);
+                                description.setText("Description: " + document.getString("description"));
+                                params.setMargins(0,150,0,0);
+                                description.setLayoutParams(params);
+                                cardView.addView(description);
+
+                                Button delete = new Button(ViewRecords.this);
+                                delete.setText("Delete");
+                                params.setMargins(700,0,0,0);
+                                delete.setLayoutParams(params);
+                                delete.setOnClickListener(cardButtons);
+                                cardView.addView(delete);
+
+                                for (QueryDocumentSnapshot d : activeDocuments)
+                                    if (d == document)
+                                    {
+                                        duplicate = true;
+                                        delete.setId( (activeDocuments.indexOf(d)) + 1 );
+                                    }
+                                if (!duplicate)
+                                {
+                                    activeDocuments.add(document);
+                                    delete.setId(id + 1);
+                                }
+
+                                cardMargin += 450;
+                                id += 1;
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 }
