c7c166cf606147071c8fb766a287b2a3d3d14fc1
Reid Giles <33459088+ReidGiles@users.noreply.github.com>
2019-05-03 04:43:41 +0100

    Unit test

---
+++ WalletWatcher/app/src/test/java/com/example/gilr1_17/walletwatcher/AddRecordTest.java
@@ -0,0 +1,22 @@
+package com.example.gilr1_17.walletwatcher;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * Unit tests for AddRecord class
+ */
+public class AddRecordTest {
+    @Test
+    public void user_isLoggedIn()
+    {
+        AddRecord addRecord = new AddRecord();
+
+        // Log in user and use assert to check log in was successful?
+
+        // addRecord.logIn();
+        //assertNotEquals(addRecord.account, null);
+    }
+}
